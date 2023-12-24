b43a1c08ae7c878a70a6a3350e82e16e2d84a4ed
Prashik Kamble <138839248+prashik536@users.noreply.github.com>
2023-12-24 23:19:27 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIARXQ6BUWLT5KAUF4Q"
-  secret_key = "NrE8x8QXzR0E+XbsFOt+r2+9tr/v5eqw1xxn3Krx"
+  access_key = "AKIARXQ6BUWLT5KAUF4Q" # use your access key
+  secret_key = "NrE8x8QXzR0E+XbsFOt+r2+9tr/v5eqw1xxn3Krx" # use your secret key
 }
